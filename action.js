eb85915f4754954c6d3b9f348b83c68be6d405b3
Maple Miao <522856232@qq.com>
2019-09-03 20:37:09 +0800

    Update action.js

--- action.js
+++ action.js
@@ -2,4 +2,6 @@ const cp = require("child_process");
 console.log(process)
 console.log(process.env)
 
+process.env["PATH"] = `maple=1:${process.env["PATH"]}`
+
 console.log(cp.execSync("node -v").toString("utf8"))

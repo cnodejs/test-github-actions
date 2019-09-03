302d31f55a0ea94e5f606e6a893d4f985eff8a03
Maple Miao <522856232@qq.com>
2019-09-03 20:44:30 +0800

    Update action.js

--- action.js
+++ action.js
@@ -1,7 +1,8 @@
 const cp = require("child_process");
+const path = require("path");
 console.log(process)
 
-process.env["PATH"] = `/home:${process.env["PATH"]}`
+process.env["PATH"] = `/home${path.delimiter}${process.env["PATH"]}`
 
 console.log("------------ env --------")
 console.log(process.env)
